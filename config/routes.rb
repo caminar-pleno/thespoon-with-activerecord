7c4e6f48f8002fc26e7218277087128beb8aedbc
Gina Vargas <caminar.pleno@gmail.com>
2021-08-29 22:33:19 -0400

    Add CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get "restaurants", to: "restaurants#index"
+  get "restaurants/new", to: "restaurants#new" # The `new` route needs to be *before* `show` route.
+  get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  post "restaurants", to: "restaurants#create"
+  get "restaurants/:id/edit", to: "restaurants#edit"
+  patch "restaurants/:id", to: "restaurants#update"
+  delete "restaurants/:id", to: "restaurants#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
